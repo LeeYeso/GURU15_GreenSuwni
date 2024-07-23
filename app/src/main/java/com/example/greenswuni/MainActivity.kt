66c17c4888c70cdb5d98784f3707428e98df1bf1
LeeYeso <leeyeso0315@gmail.com>
2024-07-23 16:41:54 +0900

    프로젝트 확인

---
+++ app/src/main/java/com/example/greenswuni/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.greenswuni
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //주석 변경 확인
+        //확인 완료하기를..
+        //push확인
+    }
+}
